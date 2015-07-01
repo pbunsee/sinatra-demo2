307c2063641bf216751b3022a43f80c9c9c54460
Jeremy Raines <jraines@gmail.com>
2015-07-01 12:25:31 -0400

    Add signup flow

--- app.rb
+++ app.rb
@@ -3,3 +3,20 @@ require 'sinatra/activerecord'
 require './models.rb'
 
 set :database, "sqlite3:nottwitter.sqlite3"
+
+get '/sign-up' do
+  erb :sign_up
+end
+
+post '/sign-up' do
+  confirmation = params[:confirm_password]
+
+  if confirmation == params[:user][:password]
+    @user = User.create(params[:user])
+    "SIGNED UP #{@user.username}"
+  else
+    "Your password & confirmation did not match, try again"
+  end
+
+  
+end
