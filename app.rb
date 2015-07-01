21ffb17dc376d9a930e949372a339086375354d8
Jeremy Raines <jraines@gmail.com>
2015-07-01 18:14:37 -0400

    Fix layout & set up signin

--- app.rb
+++ app.rb
@@ -1,11 +1,27 @@
 require 'sinatra'
 require 'sinatra/activerecord'
 require './models.rb'
+require 'bundler/setup'
+require 'rack-flash'
+
+enable :sessions
 
 set :database, "sqlite3:nottwitter.sqlite3"
 
+
 get '/sign-up' do
   erb :sign_up
+end
+
+post '/sign-in' do
+  @user = User.find_by_username(params[:username])
+  if @user.password == params[:password]
+    session[:user_id] = @user.id
+    params = nil
+    redirect '/'
+  else
+    "Invalid login"
+  end
 end
 
 post '/sign-up' do
@@ -22,4 +38,15 @@ end
 get '/' do
   @groups = Group.all
   erb :index
+end
+
+get '/sign-out' do
+  session[:user_id] = nil
+  redirect '/'
+end
+
+def current_user
+  if session[:user_id]
+    @current_user ||= User.find(session[:user_id])
+  end
 end
